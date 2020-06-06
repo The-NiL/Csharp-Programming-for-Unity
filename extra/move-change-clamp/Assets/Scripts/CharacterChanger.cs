8c464ccba0046f22d54cb0100bce8c933202bea1
Niloofar Saeedi <Niloo.saeedi78@gmail.com>
2020-06-06 18:18:59 +0430

    Add files via upload

---
+++ extra/move-change-clamp/Assets/Scripts/CharacterChanger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterChanger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject characterPrefab0;
+
+    [SerializeField]
+    GameObject characterPrefab1;
+
+    [SerializeField]
+    GameObject characterPrefab2;
+
+    [SerializeField]
+    GameObject characterPrefab3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // spawn character 0 by default after game starts
+        int randomCharacter = Random.Range(0, 4);
+        Vector3 location = Input.mousePosition;
+        location = Camera.main.ScreenToWorldPoint(location);
+
+        Instantiate<GameObject>(characterPrefab0, location, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int randomCharacter = Random.Range(0, 4);
+        Vector3 location = Input.mousePosition;
+        location = Camera.main.ScreenToWorldPoint(location);
+
+        // get all available objects in the scene with provided tag
+        GameObject[] sceneObject = GameObject.FindGameObjectsWithTag("character");
+
+        // if user righ clicks spawn a random character in the current mouse location
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (randomCharacter == 0)
+                Instantiate<GameObject>(characterPrefab0, location, Quaternion.identity);
+
+            else if (randomCharacter == 1)
+                Instantiate<GameObject>(characterPrefab1, location, Quaternion.identity);
+
+            else if (randomCharacter == 2)
+                Instantiate<GameObject>(characterPrefab2, location, Quaternion.identity);
+
+            else
+                Instantiate<GameObject>(characterPrefab3, location, Quaternion.identity);
+
+            // destroy last object after spawn one
+            Destroy(sceneObject[0]);
+        }
+        
+    }
+}
---
+++ extra/move-change-clamp/Assets/Scripts/ScreenUtils.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ScreenUtils
+{
+
+    static float screenLeft;
+    static float screenRight;
+    static float screenTop;
+    static float screenBottom;
+
+
+    public static float ScreenLeft
+    {
+        get { return screenLeft; }
+    }
+
+
+    public static float ScreenRight
+    {
+        get { return screenRight; }
+    }
+
+
+    public static float ScreenTop
+    {
+        get { return screenTop; }
+    }
+
+
+    public static float ScreenBottom
+    {
+        get { return screenBottom; }
+    }
+
+
+
+    public static void Initialize()
+    {
+        // save screen edges in world coordinates 
+        Vector3 upperRightCornerScreen = new Vector3(Screen.width, Screen.height,
+            -Camera.main.transform.position.z);
+        Vector3 upperRightCornerWorld = Camera.main.ScreenToWorldPoint(upperRightCornerScreen);
+
+        Vector3 lowerLeftCornerScreen = new Vector3(0, 0,
+            -Camera.main.transform.position.z);
+        Vector3 lowerLeftCornerWorld = Camera.main.ScreenToWorldPoint(lowerLeftCornerScreen);
+
+        screenTop = upperRightCornerWorld.y;
+        screenBottom = lowerLeftCornerWorld.y;
+        screenRight = upperRightCornerWorld.x;
+        screenLeft = lowerLeftCornerWorld.x;
+
+    }
+
+
+}
